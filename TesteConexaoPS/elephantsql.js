04043a6526d1a91983cbf6f0e148333942e87312
Renata <renata.sendreti@gmail.com>
2018-09-12 13:11:38 -0300

    Conexão do nodejs com banco de dados postgresql. Conexão bem sucedida.

---
+++ TesteConexaoPS/elephantsql.js
@@ -0,0 +1,20 @@
+var pg = require('pg');
+//or native libpq bindings
+//var pg = require('pg').native
+
+//var conString = "Server=localhost;Database=echo;User Id=usuario;Password=AchillesDying;Port=5432" //Can be found in the Details page
+var conString = "postgres://usuario:AchillesDying@localhost:5432/echo";
+var client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT * from aluno', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].nome);
+    // >> output: 2018-08-23T14:02:57.117Z
+    client.end();
+  });
+});
