8c901d431b40d6864842573a8133869dade5f042
Renata <renata.sendreti@gmail.com>
2018-10-05 20:48:05 -0300

    alterado .sql de procura de caronas

--- models/queries/carona/search_viagem_data_hora_origem_destino.sql
+++ models/queries/carona/search_viagem_data_hora_origem_destino.sql
@@ -1,2 +1,2 @@
 SELECT * FROM viagem
-where viagem.dia = $1 AND viagem.hora = $2 AND viagem.id_origem = $3 AND viagem.id_destino = $4
+where viagem.dia = $1 AND (viagem.hora between time $2 - interval '2 hours' and time $2 + interval '2 hours') AND viagem.id_origem = $3 AND viagem.id_destino = $4
