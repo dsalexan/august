def58d2b8e18d3831946184676c4aa6759b0998b
Renata <renata.sendreti@gmail.com>
2018-10-10 23:48:39 -0300

    erro na query insert corrigido

--- models/queries/carona/insert_viagem.sql
+++ models/queries/carona/insert_viagem.sql
@@ -1,2 +1,2 @@
-INSERT INTO viagem (id_motorista, dia, hora, preco, qtd_vagas, descricao) VALUES
-($1, $2, $3, $4, $5, $6)
+INSERT INTO viagem (id_motorista, dia, preco, qtd_vagas, descricao) VALUES
+($1, $2, $3, $4, $5)
