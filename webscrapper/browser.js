fec6e9669b0012d39240d31299cc904865fc8841
Andrew.Campos <a.campos98@live.com>
2018-10-15 17:57:06 -0300

    modificacoes no webscrapper

--- webscrapper/browser.js
+++ webscrapper/browser.js
@@ -5,41 +5,64 @@ const rp = require('request-promise');
 const cheerio = require('cheerio');
 
 const usuario = 'usuario';
-const senha = 'senha.intranet';
+const senha = 'senha';
 const ru = String('https://phpu.unifesp.br/ru_consulta/index.php');
 const intranet = String ('https://intranet.unifesp.br/');
 const historico = String ('https://www3.unifesp.br/prograd/app_prograd/he_novo/he_novo_blank_inicial/he_novo_blank_inicial.php?SID=fd0951e41ca63dc625082027e912ed9a&INapli=');
+const atestado = String ('https://www3.unifesp.br/prograd/app/atestados?SID=dd4f5be9a9614483f802422e739d9593&INapli=');
 const op = parseInt(1);
 
-async function getPic() { // screeshot da tela requisitada
+async function main() {
 
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(intranet);
-  await page.keyboard.press('Tab');
-  await page.keyboard.type(usuario); // insere usuário no campo
-  await page.keyboard.press('Tab');
-  await page.keyboard.type(senha); // insere senha no campo
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Enter');
-  await page.keyboard.press('Enter');
-  await page.mouse.move(200,330);
-  await page.waitFor(500); // esperar 500ms
-  await page.goto(historico);
-  await page.waitFor(500);
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Enter');
-  await page.waitFor(500);
+  try{
+    const browser = await puppeteer.launch({headless: true});
+    const page = await browser.newPage();
+    await page.goto(intranet);
+    await page.keyboard.press('Tab');
+    await page.keyboard.type(usuario); // insere usuário no campo
+    await page.keyboard.press('Tab');
+    await page.keyboard.type(senha); // insere senha no campo
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Enter');
+    await page.keyboard.press('Enter');
+    await page.mouse.move(200,330);
+    await page.waitFor(500); // esperar 500ms
+    await page.goto(historico);
+    await page.waitFor(500);
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Enter');
+    await page.waitFor(500);
+    // pega informacoes do historico
+    try{
+      const content = page.$$('.scGridFieldEven');
+      //console.log(content.length);
+      
+    }catch(e){
+      console.log('Erro em historico! ' + e);
+    }
 
-  let content = await page.content();
-  var $ = cheerio.load(content); // pega o corpo do html da página7
-  $('.scGridTabela tr').each(function(){
-    var materia = $(this).find('#id_sc_field_unidade_1').text().trim();
-    fs.appendFile('content/elementos.txt',materia + '\n');
-  });
+    await page.goto(atestado);
+    await page.waitFor(500);
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Enter');
+    await page.waitFor(1000);
+    // pega informacoes o atestado
+    /*
+    try{
+      const table = await page.$$('tr');
+      var x = table.getElementsByTagName('th')
+      console.log(x[0]);
+  
+    }
+    catch(e){
+      console.log('Erro em Atestado! ' + e);
+    }*/
 
-  await browser.close();// fecha o browser
+    await browser.close();// fecha o browser
+  }catch(e){
+    console.log('Erro na Main! ' + e);
+  }
 }
 
-getPic(); 
+main();
