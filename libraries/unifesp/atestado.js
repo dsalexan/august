b9a14b90e2c70e8888a4a8e2adea84196a1b9754
dsalexan <danilo.salexan@gmail.com>
2018-11-06 17:33:19 -0200

    atestado finalizado (ajustes necessários na questao fo full-icu

--- libraries/unifesp/atestado.js
+++ libraries/unifesp/atestado.js
@@ -11,6 +11,25 @@ const PARAGRAPH_ATESTADO_SELECTOR = '#content p'
 const LINK_ATESTADO_SELECTOR = '#content p a'
 
 const CONTENT_ATESTADO_SELECTOR = '#content p.texto:nth-of-type(3)'
+const TABLE_ATESTADO_SELECTOR = '#content table tr'
+const TABLE_ATESTADO_TRANSLATE = {
+    'CÓDIGO': 'código',
+    'LISTA DE UNIDADES CURRICULARES MATRICULADO': 'uc',
+    'TURNO': 'turno',
+    'DIA': 'dia',
+    'INÍCIO': 'inicio',
+    'TÉRMINO': 'termino',
+}
+const TIMES_ATESTADO_TRANSLATE = {
+    '8H': '08h00',
+    '10H': '10h00',
+    '13.3H': '13h30',
+    '15.3H': '15h30',
+    '19H': '19h00',
+    '21H': '21h00'
+}
+
+const PARAGRAPH2_ATESTADO_SELECTOR = 'div.hash p:nth-of-type(1)'
 
 var read_atestado = function(browser, page, options){
     return new Promise(async resolve => {
@@ -75,10 +94,61 @@ var compile_atestado = function(html){
         for(let i = 0; i <= paragraph.length - size; i++){
             perm.push(paragraph.slice(i, i + size).join(' '))
         }
-        perm = perm.map(t => [t, DateTime.fromString(t, 'dd \'de\' MMMM \'de\' yyyy', {locale:'pt-BR'})]) //.filter(dt => dt.isValid)
-        let para  = true
+        perm = perm.map(t => DateTime.fromString(t, "dd 'de' MMMM 'de' yyyy", {locale:'pt-BR'})).filter(dt => dt.isValid)
+        
+        let header = []
+        let body = []
+        $(TABLE_ATESTADO_SELECTOR).each(function(i, tr){
+            if(i == 0){
+                header = $(tr).find('th').toArray().map(th => $(th).text())
+            }else{
+                let data = {}
+                let entry = $(tr).find('td').toArray().map(th => $(th).text())
+                for(let j = 0; j < entry.length; j++){
+                    let info = entry[j]
+                    let key = TABLE_ATESTADO_TRANSLATE[header[j]]
 
-        resolve()
+                    if(key == 'inicio' || key == 'termino'){
+                        info = TIMES_ATESTADO_TRANSLATE[info]
+                    }else if(key == 'dia'){
+                        info = info.toLowerCase()
+                    }
+
+                    data[key] = info
+                }
+                body.push(data)
+            }
+        })
+
+        let paragraph2 = $(PARAGRAPH2_ATESTADO_SELECTOR)
+            .text()
+            .trim()
+            .replace(/(\t)+/g, ' ')
+            .replace(/(\n)+/g, ' ')
+            .replace(/(,)+/g, ' ')
+            .replace(/(-)+/g, ' ')
+            .replace(/(\.)+/g, ' ')
+            .replace(/(   )/g, ' ')
+            .replace(/(  )/g, ' ')
+            .split(' ')
+
+        size = 2
+        validade = []
+        for(let i = 0; i <= paragraph2.length - size; i++){
+            validade.push(paragraph2.slice(i, i + size).join(' '))
+        }
+        validade = validade.map(t => DateTime.fromString(t, "dd/MM/yyyy hh:mm:ss")).filter(dt => dt.isValid)[0]
+        validade = validade.plus({days: 30})
+
+        // TODO: store as UTC
+        resolve({
+            expiration: validade.toString(),
+            semester: {
+                start: perm.length > 0 && perm[0].toString(),
+                end: perm.length > 0 && perm[1].toString()
+            },
+            classes: body
+        })
     })
 }
 
@@ -98,7 +168,7 @@ var fetch_atestado = function(browser, page, options){
         let atestado = await read_atestado(browser, page)
         options.puppeteerObject && options.puppeteerObject.destroy(options, browserPersistence)
         
-        await compile_atestado(atestado.html)
+        atestado = await compile_atestado(atestado.html)
         browserPersistence.puppeteer && (atestado.puppeteer = browserPersistence.puppeteer)
 
         await save_atestado(atestado)
