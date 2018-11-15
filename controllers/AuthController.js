1f73437057ed7d78217b5dd88f660d31f14c2af3
gucollaco <gustavo.m.collaco@gmail.com>
2018-11-15 15:11:37 -0200

    more conflics

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -114,7 +114,6 @@ router.get('/login', function(req, res){
         console.log(`Login successful for <${user.login_intranet}> with token: ${token}`)
         res.status(200).send({
             auth: true,
-<<<<<<< HEAD
             data: {
                 ra: user.ra_aluno,
                 nome: user.nome,
@@ -123,9 +122,6 @@ router.get('/login', function(req, res){
                 email: user.email,
                 telefone: user.telefone
             },
-=======
-            data: user,
->>>>>>> db1a54787268648c342bf877ac93fd4843892597
             token: token
         })
 
