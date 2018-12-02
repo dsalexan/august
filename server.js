69b21ffedff5a462da30d5158c9e46f76eeda7e1
dsalexan <danilo.salexan@gmail.com>
2018-12-02 00:40:04 -0200

    polyfill de gambi pra funcionar

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ router.get('/api/caronas/put/viagem/aumenta_vaga', Carona.updateMaisVaga)
 // Divulgacao
 router.get('/api/divulgacao/delete/divulgacao', Divulgacao.remove_divulgacao)
 router.get('/api/divulgacao/post/divulgacao', Divulgacao.insert_divulgacao)
-router.get('/api/divulgacao/get/divulgacao/diahora', Divulgacao.busca_divulgacao_dia_hora)
+router.get('/api/divulgacao/get/divulgacao/diahora', Divulgacao.busca_divulgacao_dia_hora_inicio)
 router.get('/api/divulgacao/get/divulgacao/dia', Divulgacao.busca_divulgacao_dia)
 router.get('/api/divulgacao/get/divulgacao/hora', Divulgacao.busca_divulgacao_hora)
 router.get('/api/divulgacao/get/divulgacao/tipodiahora', Divulgacao.busca_divulgacao_tipo_dia_hora) 
