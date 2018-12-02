47b1e117daa4f6969ab0093d57b6dcee214b086e
dsalexan <danilo.salexan@gmail.com>
2018-12-02 01:14:10 -0200

    cors 6th

--- server.js
+++ server.js
@@ -31,14 +31,16 @@ var Alunos = require('./models/Alunos')
 
 // log request middleware
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    if ('OPTIONS' == req.method) {
+       res.sendStatus(200);
+    } else {
+       next();
+    }
+    
     console.log(req.method, req.url)
-
-    next()
 })
 
 var bodyParser = require('body-parser')
