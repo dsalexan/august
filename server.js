7ac7d5b66a480eb9b7217978c337cf344c0bab16
dsalexan <danilo.salexan@gmail.com>
2018-12-02 01:24:28 -0200

    cors 7th

--- server.js
+++ server.js
@@ -32,14 +32,14 @@ var Alunos = require('./models/Alunos')
 // log request middleware
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header("Access-Control-Allow-Headers", "*");
     if ('OPTIONS' == req.method) {
        res.sendStatus(200);
     } else {
        next();
     }
-    
+
     console.log(req.method, req.url)
 })
 
