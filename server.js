024aa18a6931ab0b96972a224526a09a1bce0e5d
dsalexan <danilo.salexan@gmail.com>
2018-12-02 01:04:59 -0200

    cors 4th

--- server.js
+++ server.js
@@ -36,9 +36,10 @@ router.use(bodyParser.json())
 
 // log request middleware
 router.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
     console.log(req.method, req.url)
 
