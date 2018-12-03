ff777531bd77e07bfb07166c52663764a4677499
gucollaco <gustavo.m.collaco@gmail.com>
2018-12-03 15:04:58 -0200

    .

--- server.js
+++ server.js
@@ -222,6 +222,10 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     next()
 })
+
+app.use(cors())
+app.options('*', cors())
+//
 // app.use(function(req, res, next) {
 //     console.log(req.method, req.url)
 
